e33a7f4eee44e3209165612af4bd5be5931f5f60
AwsAdmin <lucky.patil92@gmail.com>
2025-10-06 22:54:43 +0530

    # updated output file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,3 @@
-output "public_ip" {
-  description = "Public IP of the web EC2 instance"
-  value       = aws_instance.web.public_ip
-}
-
-
 output "public_ips" {
   description = "Public IPs of the web EC2 instances"
   value       = [for instance in aws_instance.web : instance.public_ip]
@@ -13,6 +7,3 @@ output "public_dns" {
   description = "Public DNS of the web EC2 instances"
   value       = [for instance in aws_instance.web : instance.public_dns]
 }
-- name: Import existing key pair (ignore errors)
-  run: terraform import aws_key_pair.deployer netlink-deployer-key || true
-#
